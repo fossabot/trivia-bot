72d4e621a4de572644cb198f1da3252932ec3e90
Evan Gubarev <evan@evangubarev.com>
2020-06-14 19:51:32 -0700

    Added topgg cog

--- bot.py
+++ bot.py
@@ -450,4 +450,7 @@ async def on_ready():
     global triviatoken
     triviatoken = urllib.parse.unquote(loads(n)['token'])
     print(triviatoken)
+
+bot.load_extension("cogs.topgg")
+
 client.run(TOKEN)
