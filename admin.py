f75d10c7fc348029c0e89ec638fac9042e4bc39f
Evan Gubarev <evchik2007@gmail.com>
2020-06-28 11:27:57 -0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,3 +1,25 @@
+ # -*- coding: utf8 -*-
+
+ # 
+ # This file is part of the Trivia Bot distribution (https://github.com/gubareve/trivia-bot).
+ # Copyright (c) 2015 gubareve.
+ # 
+ # This program is free software: you can redistribute it and/or modify  
+ # it under the terms of the GNU General Public License as published by  
+ # the Free Software Foundation, version 3.
+ #
+ # This program is distributed in the hope that it will be useful, but 
+ # WITHOUT ANY WARRANTY; without even the implied warranty of 
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ # General Public License for more details.
+ #
+ # To comply with the credit section of the GNU license, anyone 
+ # modifying this file must not remove the credit command.
+ #
+ # You should have received a copy of the GNU General Public License 
+ # along with this program. If not, see <http://www.gnu.org/licenses/>.
+ #
+
 import smtplib
 import discord
 from operator import itemgetter
