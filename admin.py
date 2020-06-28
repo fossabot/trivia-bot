2566da8361357c3c7279d8053678f794be7b6f05
Evan Gubarev <evchik2007@gmail.com>
2020-06-28 11:30:47 -0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@
 
  # 
  # This file is part of the Trivia Bot distribution (https://github.com/gubareve/trivia-bot).
- # Copyright (c) 2015 gubareve.
+ # Copyright (c) 2020 gubareve.
  # 
  # This program is free software: you can redistribute it and/or modify  
  # it under the terms of the GNU General Public License as published by  
